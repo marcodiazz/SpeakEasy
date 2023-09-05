d3eff13a034dec48ec59946858e17cb9e5c39fdb
marcodiazz <75041809+marcodiazz@users.noreply.github.com>
2023-09-05 16:06:17 +0200

    assets moved to public

--- src/components/PrizeCard.tsx
+++ src/components/PrizeCard.tsx
@@ -1,8 +1,7 @@
 export function PrizeCard (props){
-    const baseUrl = '/src/assets/';
     return (
         <div className='prizes__prize'>
-            <img className='prizes__imgPrize' src={baseUrl + props.link} alt="" />
+            <img className='prizes__imgPrize' src={props.link} alt="" />
             <h2>{props.name}</h2>
             <h3 className='pinkText'>{props.points} points</h3>
         </div>
--- src/pages/Prizes.tsx
+++ src/pages/Prizes.tsx
@@ -5,16 +5,16 @@ export function Prizes(){
     <div className='prizes__container'>
         <h1>Claim your <span className='pinkText'>prize</span>!</h1>
         <div className='prizes__row'>
-            <PrizeCard link="chupachups.png" name="Chupachups" points={1000}/>
-            <PrizeCard link="cafe.jpg" name="Cafe" points={1500}/>
+            <PrizeCard link="/public/chupachups.png" name="Chupachups" points={1000}/>
+            <PrizeCard link="/public/cafe.jpg" name="Cafe" points={1500}/>
         </div>
         <div className='prizes__row'>
-            <PrizeCard link="crema_manos.jpeg" name="Crema" points={2000}/>
-            <PrizeCard link="karmex.jpeg" name="Carmex" points={4000}/>
+            <PrizeCard link="/public/crema_manos.jpeg" name="Crema" points={2000}/>
+            <PrizeCard link="/public/karmex.jpeg" name="Carmex" points={4000}/>
         </div>
         <div className='prizes__row'>
-            <PrizeCard link="chaqueta.png" name="Chaqueta" points="10k"/>
-            <PrizeCard link="iphone.jpeg" name="Iphone" points="1m"/>
+            <PrizeCard link="/public/chaqueta.png" name="Chaqueta" points="10k"/>
+            <PrizeCard link="/public/iphone.jpeg" name="Iphone" points="1m"/>
         </div>
     </div>
   )
